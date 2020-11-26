617504dadb760f2b7c447053e4cd36d78c8e772b
TrojanFi <filip23t@gmail.com>
2020-11-26 21:00:14 +0100

    Update AppWebSecurityConfig.java

--- app/src/main/java/pl/edu/pjwstk/jaz/Authorization/AppWebSecurityConfig.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/Authorization/AppWebSecurityConfig.java
@@ -14,6 +14,8 @@ public class AppWebSecurityConfig {
         FilterRegistrationBean<ExampleFilter> registrationBean = new FilterRegistrationBean<>();
 
         registrationBean.setFilter(new ExampleFilter(userSession));
+        registrationBean.addUrlPatterns("/users");
+        registrationBean.addUrlPatterns("/admin");
         registrationBean.addUrlPatterns("/auth0/*");
 
         return  registrationBean;
